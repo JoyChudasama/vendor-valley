026a44bc3ee312c774a967a37d7d8c2dd06ac0c2
JoyChudasama <joychudasama11@gmail.com>
2023-03-12 18:58:25 -0300

    added email and password match validation in registration; added login link to registration form

---
+++ assets/controllers/user_registration_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from '@hotwired/stimulus';
+import { isSame } from '../js/helper';
+
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+
+    static targets = ['emailInput', 'confirmEmailInput', 'passwordInput', 'confirmPasswordInput', 'submitButton']
+
+    matchEmail() {
+        const doesEmailMatch = isSame(this.emailInputTarget.value, this.confirmEmailInputTarget.value)
+
+        if (!doesEmailMatch) {
+            this.confirmEmailInputTarget.classList.add('is-invalid');
+            this.submitButtonTarget.setAttribute('disabled', '');
+            return;
+        }
+
+        this.confirmEmailInputTarget.classList.remove('is-invalid');
+        this.submitButtonTarget.removeAttribute('disabled');
+    }
+
+    matchPassword() {
+        const doesEmailMatch = isSame(this.passwordInputTarget.value, this.confirmPasswordInputTarget.value)
+
+        if (!doesEmailMatch) {
+            this.confirmPasswordInputTarget.classList.add('is-invalid');
+            this.submitButtonTarget.setAttribute('disabled', '');
+            return;
+        }
+
+        this.confirmPasswordInputTarget.classList.remove('is-invalid');
+        this.submitButtonTarget.removeAttribute('disabled');
+    }
+}
---
+++ assets/js/helper.js
@@ -0,0 +1,5 @@
+const isSame = (val1, val2) => {
+    return val1 === val2;
+}
+
+export { isSame };
