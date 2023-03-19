5fd450ab1f27c59d51de84a591735c2230eab576
JoyChudasama <joychudasama11@gmail.com>
2023-03-19 00:39:27 -0300

    added vendor dashboard/profile; updated profile controller; made Vendor CRUD; working on making product and productImage crud

--- assets/controllers/profile_controller.js
+++ assets/controllers/profile_controller.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 
 export default class extends Controller {
 
-    static targets = ['form', 'editButton', 'submitButton', 'cancelButton']
+    static targets = ['form', 'editButton', 'submitButton', 'cancelButton', 'deleteButton']
 
     connect() {
         this.disableAllInputs();
@@ -13,39 +13,41 @@ export default class extends Controller {
 
         e.preventDefault();
 
-        const inputs = $(this.formTarget).find('input');
         const dropdowns = $(this.formTarget).find('select');
-
-        inputs.each((i, e) => {
-            e.removeAttribute('disabled');
-        });
+        const textFields = $('input[type=text], textarea');
 
         dropdowns.each((i, e) => {
+            e.removeAttribute('disabled');
+        });
+       
+        textFields.each((i, e) => {
             e.removeAttribute('disabled');
         });
 
         this.editButtonTarget.classList.add('d-none');
         this.hasSubmitButtonTarget && this.submitButtonTarget.classList.remove('d-none');
         this.cancelButtonTarget.classList.remove('d-none');
+        this.hasDeleteButtonTarget && this.deleteButtonTarget.classList.remove('d-none');
     }
 
     disableAllInputs(e) {
 
         e && e.preventDefault();
 
-        const inputs = $(this.formTarget).find('input');
         const dropdowns = $(this.formTarget).find('select');
-
-        inputs.each((i, e) => {
-            e.setAttribute('disabled', '');
-        });
+        const textFields = $('input[type=text], textarea');
 
         dropdowns.each((i, e) => {
             e.setAttribute('disabled', '');
         });
+ 
+        textFields.each((i, e) => {
+            e.setAttribute('disabled','');
+        });
 
         this.hasSubmitButtonTarget && this.submitButtonTarget.classList.add('d-none');
         this.cancelButtonTarget.classList.add('d-none');
+        this.hasDeleteButtonTarget && this.deleteButtonTarget.classList.add('d-none');
         this.editButtonTarget.classList.remove('d-none');
     }
 }
---
+++ src/Controller/VendorController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Vendor;
+use App\Form\VendorType;
+use App\Repository\VendorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/vendor')]
+class VendorController extends AbstractController
+{
+
+    #[Route('/{id}/edit', name: 'app_vendor_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Vendor $vendor, VendorRepository $vendorRepository): Response
+    {
+        $form = $this->createForm(VendorType::class, $vendor, [
+            'action' => $this->generateUrl('app_vendor_profile', ['id' => $vendor->getId()])
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $vendorRepository->save($vendor, true);
+
+            return new Response(null, 204);
+        }
+
+        return $this->render('vendor/profile_content.html.twig', [
+            'vendor' => $vendor,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/profile-{id}', name: 'app_vendor_profile', methods: ['GET', 'POST'])]
+    public function show(Vendor $vendor): Response
+    {
+        $form = $this->createForm(VendorType::class, $vendor, [
+            'action' => $this->generateUrl('app_vendor_profile', ['id' => $vendor->getId()])
+        ]);
+
+        return $this->render('vendor/profile.html.twig', [
+            'vendor' => $vendor,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('/{id}', name: 'app_vendor_delete', methods: ['POST'])]
+    public function delete(Request $request, Vendor $vendor, VendorRepository $vendorRepository): Response
+    {
+        $user = $vendor->getUser();
+
+        if ($this->isCsrfTokenValid('delete' . $vendor->getId(), $request->request->get('_token'))) {
+            $user->setBecomeVendor(null);
+            $vendorRepository->remove($vendor, true);
+        }
+
+        return $this->redirectToRoute('app_user_profile', ['id'=>$user->getId()], Response::HTTP_SEE_OTHER);
+    }
+}
